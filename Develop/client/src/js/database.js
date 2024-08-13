cee7cc6db0085fb5e6cca6ab4f56c38eafc87aae
Renee Rodriguez <poulin.reneemarie@gmail.com>
2024-08-13 17:35:43 -0400

    Implemented text editor.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,34 @@
 import { openDB } from 'idb';
+const DBNAME = 'jate';
+// const STORENAME = 'content';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB(DBNAME, 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains(DBNAME)) {
+        console.log(`${DBNAME} database already exists`);
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore(DBNAME, { keyPath: 'id', autoIncrement: true });
+      console.log(`${DBNAME} database created`);
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  if (typeof content !== 'string') {
+    throw new TypeError('Content must be a string');
+  }
+  const db = await openDB(DBNAME, 1);
+  await db.put(DBNAME, { content });
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB(DBNAME, 1);
+  const result = await db.getAll(DBNAME);
+  console.log('data retrieved from the database', result);
+  return result;
+};
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,27 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let userPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (ev) => {
+  ev.preventDefault();
+  userPrompt = ev;
+  butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  if (userPrompt) {
+    userPrompt.prompt();
+    const { outcome } = await userPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+    userPrompt = null;
+    butInstall.style.display = 'none';
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (ev) => {
+  console.log('PWA was installed', ev);
+});
